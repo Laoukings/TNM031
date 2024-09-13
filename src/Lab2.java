2fb9a0f42b95e43d9a0956b83a97ad8ed800f224
Laoukings <Lukpe828@student.liu.se>
2024-09-13 16:31:33 +0200

    final

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,8 +1,7 @@
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Random; 
-
+import java.util.Random;
 
 public class Lab2 {
 
@@ -12,74 +11,45 @@ public class Lab2 {
 
         Random genVal = new Random();
         // middle value is % probability to be a prime value?
+        //Constructs a randomly generated positive BigInteger that is probably prime, with the specified bitLength.
         BigInteger p = new BigInteger(bitLength, 100, genVal);
         BigInteger q = new BigInteger(bitLength, 100, genVal);
         //BigInteger g = BigInteger.probablePrime(bitLength, genVal); //same thing as above
-
-        System.out.println("är");
-        // System.out.println(q);
-        //System.out.println(g);
 
         //n = pq
         BigInteger n = p.multiply(q);
 
         //generate gcd(e, p-1, q-1)
         BigInteger fard = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        //System.out.println(fard);
 
-
-        int ttt = 0;
         //find gcd of fard to get e 
-        BigInteger e = fard.subtract(BigInteger.ONE); 
-        while (e.gcd(fard).equals(BigInteger.ONE)) { //shid gets stuck here not correct go fix later :))
+        BigInteger e = fard.subtract(BigInteger.ONE);
+
+        while (!e.gcd(fard).equals(BigInteger.ONE)) {
             e.subtract(BigInteger.ONE);
-            ttt++;
-            System.out.println(ttt);
+
         }
-        
+
         // d = e^-1 mod(fard);
         BigInteger d = e.modInverse(fard);
 
-
         //Alice encrypts message
-        String aliceMessage = "papi santana";
+        String aliceMessage = "This is secret information! ";
         BigInteger m = new BigInteger(aliceMessage.getBytes());
 
         //encrypted message
         BigInteger c = m.modPow(e, n);
 
-        System.out.println("paj");
-
         //bob decrypts message
         m = c.modPow(d, n);
+        String decryptedmessage = new String(m.toByteArray());
 
         //output
-        System.out.println("Sauce");
         System.out.println("Message:" + aliceMessage);
         System.out.println("Encrypted Message:" + c.toString());
-        System.out.println("Decrypted message: " + m.toString());
+        System.out.println("Decrypted message: " + (decryptedmessage));
 
-
-        // to convert an integer b into a BigInteger
-
-        /*
-        
-        --Copy pasted
-
-        int b = 170;
-        BigInteger bigB = new BigInteger(String.valueOf(b));
-        System.out.println("Hello, World!");
-
-        //	 	to read a string from keyboard
-        String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-
-        //	 	to convert a string s into a BigInteger
-        String s = "abc";
-        BigInteger c = new BigInteger(s.getBytes());
-
-        // to convert a BigInteger back to a string	
-        BigInteger a;
-        //String s = new String(a.toByteArray());
-         */
     }
 
 }
