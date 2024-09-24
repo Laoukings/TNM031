8acc25d2450f4d2ea8cc291db13dc4503cec85e0
Magnus <91549385+KoksILasten@users.noreply.github.com>
2024-09-24 17:52:07 +0200

    Skibidi lab3

--- Lab3/src/client/SecureAdditionClient.java
+++ Lab3/src/client/SecureAdditionClient.java
@@ -4,22 +4,24 @@ import java.io.*;
 import java.net.*;
 import java.security.KeyStore;
 import javax.net.ssl.*;
+import java.util.*;
 
 public class SecureAdditionClient {
 
     private InetAddress host;
     private int port;
-    // This is not a reserved port number 
+    // This is not a reserved port number
     static final int DEFAULT_PORT = 8189;
-    //static final String KEYSTORE = "LIUkeystore.ks";
-    //static final String TRUSTSTORE = "LIUtruststore.ks";
+    // static final String KEYSTORE = "LIUkeystore.ks";
+    // static final String TRUSTSTORE = "LIUtruststore.ks";
     static final String KEYSTOREPASS = "123456";
     static final String TRUSTSTOREPASS = "abcdef";
 
     static final String KEYSTORE = "Lab3\\src\\client\\LIUkeystore.ks";
     static final String TRUSTSTORE = "Lab3\\src\\client\\LIUtruststore.ks";
 
-    // Constructor @param host Internet address of the host where the server is located
+    // Constructor @param host Internet address of the host where the server is
+    // located
     // @param port Port number on the host where the server is listening
     public SecureAdditionClient(InetAddress host, int port) {
         this.host = host;
@@ -52,12 +54,82 @@ public class SecureAdditionClient {
             socketIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
             PrintWriter socketOut = new PrintWriter(client.getOutputStream(), true);
 
-            String numbers = "1.2 3.4 5.6";
-            System.out.println(">>>> Sending the numbers " + numbers + " to SecureAdditionServer");
-            socketOut.println(numbers);
-            System.out.println(socketIn.readLine());
+            System.out.println("Select option\nOption 1: download\nOption 2: upload\nOption 3: delete");
 
-            socketOut.println("");
+            Scanner scan = new Scanner(System.in);
+            String option = scan.nextLine();
+            String filename = "";
+
+            socketOut.println(option);
+            switch (option) {
+                case "1": // download
+                    System.out.println("Selected download\nWhich file do you want to download");
+                    filename = scan.nextLine();
+                    socketOut.println(filename);
+
+                    try {
+                        // String serverFilename = socketIn.readLine();
+                        File downloadedFile = new File("./Lab3/src/client/files/" + filename);
+                        System.out.println("downloading file: " + filename);
+                        downloadedFile.createNewFile();
+
+                        FileWriter writer = new FileWriter(downloadedFile);
+                        String line1;
+
+                        while ((line1 = socketIn.readLine()) != null) {
+                            writer.write(line1 + "\n");
+                        }
+
+                        writer.close();
+                    } catch (Exception e) {
+                        System.out.println("Skibidi");
+                        e.printStackTrace();
+                    }
+
+                    // sending server the name of the file
+                    // socketOut.println(filename);
+
+                    // about to recive the file from the server
+                    // String line;
+
+                    break;
+                case "2": // upload
+                    System.out.println("Selected upload\nWhich file do you want to upload");
+                    filename = scan.nextLine();
+                    socketOut.println(filename);
+
+                    File file = new File("./Lab3/src/client/files/");
+
+                    BufferedReader reader = new BufferedReader(new FileReader(file + "/" + filename));
+                    String line2;
+                    while ((line2 = reader.readLine()) != null) {
+                        System.out.println(line2);
+                        socketOut.println(line2);
+                    }
+                    reader.close();
+
+                    break;
+                case "3": // delete
+                    System.out.println("Selected delete\nWhich file do you want to delete");
+                    filename = scan.nextLine();
+                    socketOut.println(filename);
+
+                    System.out.println("Response: " + socketIn.readLine());
+                    break;
+
+                default:
+                    System.out.println("default case bozo");
+                    break;
+            }
+            scan.close();
+            socketOut.close();
+            // String numbers = "1.2 3.4 5.6";
+            // System.out.println(">>>> Sending the numbers " + numbers + " to
+            // SecureAdditionServer");
+            // socketOut.println(numbers);
+            // System.out.println(socketIn.readLine());
+
+            // socketOut.println("");
         } catch (Exception x) {
             System.out.println(x);
             x.printStackTrace();
@@ -83,3 +155,8 @@ public class SecureAdditionClient {
         }
     }
 }
+
+// trustor holds the place where you validate your keystore authentication
+// getsupportedciphersuites holds the type of encypting the cliet/server has
+// setEnabledciphersuites tells the client or server which ciphers it supports
+// so the client/server can choose.
--- Lab3/src/server/SecureAdditionServer.java
+++ Lab3/src/server/SecureAdditionServer.java
@@ -11,15 +11,15 @@ public class SecureAdditionServer {
     private int port;
     // This is not a reserved port number
     static final int DEFAULT_PORT = 8189;
-    //static final String KEYSTORE = "LIUkeystore.ks";
-    //static final String TRUSTSTORE = "LIUtruststore.ks";
+    // static final String KEYSTORE = "LIUkeystore.ks";
+    // static final String TRUSTSTORE = "LIUtruststore.ks";
     static final String KEYSTOREPASS = "123456";
     static final String TRUSTSTOREPASS = "abcdef";
-    
+
     // relative path to file
     static final String KEYSTORE = "Lab3\\src\\server\\LIUkeystore.ks";
     static final String TRUSTSTORE = "Lab3\\src\\server\\LIUtruststore.ks";
-    
+
     /**
      * Constructor
      *
@@ -58,28 +58,78 @@ public class SecureAdditionServer {
             BufferedReader in = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
             PrintWriter out = new PrintWriter(incoming.getOutputStream(), true);
 
-            String str;
-            while (!(str = in.readLine()).equals("")) {
-                double result = 0;
-                StringTokenizer st = new StringTokenizer(str);
-                try {
-                    while (st.hasMoreTokens()) {
-                        // Double(String) is deprecated
-                        Double d = new Double(st.nextToken());
-                        //Double d = new valueOf(st.nextToken());
-                        result += d.doubleValue();
+            String str = in.readLine();
+            String line;
+
+            switch (str) {
+                case "1": // download
+                    System.out.println("Download selected ");
+                    try {
+
+                        String fileOption = in.readLine();
+                        BufferedReader reader = new BufferedReader(
+                                new FileReader("./Lab3/src/server/files/" + fileOption));
+                        System.out.println("File name: " + fileOption);
+                        while ((line = reader.readLine()) != null) {
+                            System.out.println(line);
+                            out.println(line);
+                        }
+                        reader.close();
+                        out.close();
+                    } catch (Exception e) {
+                        System.out.println("File does probably not exist");
+                        e.printStackTrace();
                     }
-                    out.println("The result is " + result);
-                } catch (NumberFormatException nfe) {
-                    out.println("Sorry, your list contains an invalid number");
-                }
+
+                    break;
+                case "2": // upload
+                    System.out.println("upload selected");
+                    try {
+                        File uploadedFile = new File("./Lab3/src/server/files/" + in.readLine());
+                        System.out.println("File name: " + uploadedFile);
+                        uploadedFile.createNewFile();
+                        FileWriter whritehead = new FileWriter(uploadedFile);
+
+                        while ((line = in.readLine()) != null) {
+                            whritehead.write(line + "\n");
+
+                        }
+                        whritehead.close();
+                    } catch (Exception e) {
+                        System.out.println("FUCK");
+                        e.printStackTrace();
+                    }
+                    break;
+                case "3": // delete
+                    System.out.println("delete selected");
+                    String deleteFile = in.readLine();
+                    try {
+                        File file = new File("./Lab3/src/server/files/" + deleteFile);
+                        if (file.delete())
+                            out.println("The file " + deleteFile + " has been deleted from the server");
+                        else
+                            out.print("File " + deleteFile + " was not found or is already deleted");
+
+                    } catch (Exception e) {
+                        System.err.println("Delete failed kys");
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                default:
+                    System.out.println("Case defaulted closing connection");
+                    break;
             }
-            //closes the connection after calculating the sum
+
             incoming.close();
-        } catch (Exception x) {
-            System.out.println(x);
-            x.printStackTrace();
+            in.close();
+
+        } catch (Exception e) {
+            System.out.println("Server Error ");
+            e.printStackTrace();
         }
+
     }
 
     /**
